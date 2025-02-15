d6f3bc3235d935284ea1ffb1035c7639a366b237
AnthonyHaoNguyen <ann028@ucsd.edu>
2025-02-15 14:18:32 -0800

    please look at the tasks class

---
+++ lib/src/main/java/edu/ucsd/cse110/habitizer/lib/domain/Tasks.java
@@ -0,0 +1,33 @@
+package edu.ucsd.cse110.habitizer.lib.domain;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.firebase.crashlytics.buildtools.reloc.com.google.common.base.Objects;
+
+import java.util.Objects;
+
+public class Tasks {
+    private final @Nullable Integer id;
+    private final @NonNull String task;
+    private Tasks Objects;
+
+    public Tasks(@Nullable Integer id, @NonNull String task){
+        this.id = id;
+        this.task = task;
+    }
+
+    public @Nullable Integer id(){return id;}
+
+    public @NonNull String task(){return task;}
+
+
+    public boolean equals(Object o){
+        if(this==o) return true;
+        if(o == null || getClass()!=o.getClass()) return false;
+        Tasks tasks = (Tasks) o;
+        return Objects.equals(id, tasks.id) && Objects.equals(task, tasks.task);
+    }
+
+    public int hashcode() {return Objects.hash(id, task);}
+}
