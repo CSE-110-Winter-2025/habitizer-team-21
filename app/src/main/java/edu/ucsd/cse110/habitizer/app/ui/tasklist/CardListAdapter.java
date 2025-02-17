211a561cb96f2cdb9da82a25d31fd3e952fc290a
AnthonyHaoNguyen <ann028@ucsd.edu>
2025-02-16 16:26:55 -0800

    checked

--- app/src/main/java/edu/ucsd/cse110/habitizer/app/ui/tasklist/CardListAdapter.java
+++ app/src/main/java/edu/ucsd/cse110/habitizer/app/ui/tasklist/CardListAdapter.java
@@ -6,11 +6,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.graphics.Paint;
 
 import androidx.annotation.NonNull;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.HashSet;
 import java.util.function.Consumer;
 
 import edu.ucsd.cse110.habitizer.app.databinding.ListItemCardBinding;
@@ -18,6 +20,7 @@ import edu.ucsd.cse110.habitizer.lib.domain.Task;
 
 public class CardListAdapter extends ArrayAdapter<Task> {
 
+    private final HashSet<Integer> struckThroughTasks = new HashSet<>();
     public CardListAdapter(Context context, List<Task> tasks) {
         super(context, 0, new ArrayList<>(tasks)); // Ensuring a mutable list
     }
@@ -39,6 +42,27 @@ public class CardListAdapter extends ArrayAdapter<Task> {
 
         // Set task text
         binding.Task.setText(task.task());
+
+        // Applying strikethrough if the task was previously clicked on
+        if (struckThroughTasks.contains(position)) {
+            binding.Task.setPaintFlags(binding.Task.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+        } else {
+            binding.Task.setPaintFlags(binding.Task.getPaintFlags() & (~Paint.STRIKE_THRU_TEXT_FLAG));
+        }
+
+
+        // Toggle strikethrough on click for each task
+        binding.getRoot().setOnClickListener(v -> {
+            if (struckThroughTasks.contains(position)) {
+                struckThroughTasks.remove(position);
+                binding.Task.setPaintFlags(binding.Task.getPaintFlags() & (~Paint.STRIKE_THRU_TEXT_FLAG));
+            } else {
+                struckThroughTasks.add(position);
+                binding.Task.setPaintFlags(binding.Task.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+            }
+        });
+
+
 
         return binding.getRoot();
     }
