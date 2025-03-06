bf41f47b6bc983485d6c370e619b8ee96f50c840
Ashhal Usmani <ashhal.s.usmani@gmail.com>
2025-03-06 15:58:41 -0800

    Bug fixes

--- lib/src/test/java/edu/ucsd/cse110/habitizer/lib/domain/TaskTest.java
+++ lib/src/test/java/edu/ucsd/cse110/habitizer/lib/domain/TaskTest.java
@@ -50,14 +50,14 @@ public class TaskTest {
     }
     @Test
     public void testRenameTask(){ //tests if renaming works
-        var task = new Task(1,"Old", 0);
+        var task = new Task(1,"Old", 0,0);
         task.renameTask("New");
         assertEquals("New", task.task());
     }
 
     @Test
     public void testDeleteTask(){ //test if delete works
-        var task = new Task(1, "Task To Delete",0);
+        var task = new Task(1, "Task To Delete",0,0);
         assertFalse(task.isDeleted());
         task.deleteTask();
         assertTrue(task.isDeleted());
